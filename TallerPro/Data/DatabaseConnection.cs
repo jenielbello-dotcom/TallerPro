36ddf3ca68a8aba1e3639aac617cad5687a48f6f
Bello <jenielbello@gmail.com>
2026-09-08 09:43:09 -0400

    Initial commit - TallerPro conectado a BD

---
+++ TallerPro/Data/DatabaseConnection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace TallerPro.Data
+{
+    public class DatabaseConnection
+    {
+        private readonly string connectionString =
+            "Server=localhost\\SQLSERVER;Database=TallerPro1.0;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    
+
+          public bool TestConnection()
+        {
+            try
+            {
+                using SqlConnection connection = GetConnection();
+                connection.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ TallerPro/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using global ::TallerPro.Data;
+using System;
+
+namespace TallerPro
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            DatabaseConnection db = new DatabaseConnection();
+
+            if (db.TestConnection())
+                MessageBox.Show("✅ Conexión exitosa a la base de datos.");
+            else
+                MessageBox.Show("❌ Error al conectar con la base de datos.");
+        }
+    }
+}
+
+
